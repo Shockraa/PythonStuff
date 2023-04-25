33ca1b1282c26125260b74842006f1fa9738fa52
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-04-24 23:27:38 -0300

    Add files via upload

---
+++ Ex1-arearetangulo.py
@@ -0,0 +1,10 @@
+largura = float(input("Insira a largura do retângulo: "))
+comprimento = float(input("Insira o comprimento do retângulo: "))
+
+area = largura * comprimento
+volume = area * largura * comprimento
+
+print(f"A área do retângulo é {area} cm^2")
+print(f"E o volume do retângulo é {volume} cm^3")
+
+input()
